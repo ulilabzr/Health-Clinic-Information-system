2000138213ad412a7e127d7f8b58f26723e66c87
ulilabzr <absoruu@gmail.com>
2024-12-30 23:09:26 +0700

    entity doctor

---
+++ HEALTH CLINIC INFORMATION SYSTEM/Model/Entity/Doctor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEALTH_CLINIC_INFORMATION_SYSTEM.Model.Entity
+{
+    public class Doctor
+    {
+        public int DoctorId { get; set; }
+        public string Name { get; set; }
+        public string Specialization { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public int? ScheduleId { get; set; }
+
+        // Navigation properties
+        //public virtual PracticeSchedule Schedule { get; set; }
+        //public virtual ICollection<MedicalRecord> MedicalRecords { get; set; }
+    }
+}
